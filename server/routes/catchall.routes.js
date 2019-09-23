511c970fc7dd4213f853de0ac119fd385242e583
Michael Kemper <michaelkemper31@gmail.com>
2019-09-23 16:09:47 -0700

    Catchall route correction

--- server/routes/catchall.routes.js
+++ server/routes/catchall.routes.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const path = require('path');
 
 module.exports = router.all('*', (_request, response) => {
-  response.sendFile(path.resolve('dist/books/index.html'));
+  response.sendFile(path.resolve('dist/funday/index.html'));
 });
