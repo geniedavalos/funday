5689c2f04a4a7b5dab38ca3e8a0ab4848aa7e412
Wade Allen <wade.allen11@gmail.com>
2019-09-23 15:21:55 -0700

    modified database.js file to clean up lines 6 and 7

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fundaydb', {useNewUrlParser: true});
 fs.readdirSync(path.join(__dirname, './../models')).forEach(function(file) {
-    if(file.indexOf('.js') >= 0) {
-        require(path.join(__dirname, './../models') + '/' + file);
+    if(file.endsWith('.js')) {
+        require(path.join(__dirname, './../models', file));
     }
 });
