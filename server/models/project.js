79569f2a04176a6a2f5bc9801aaccdb9ee42260c
Wade Allen <wade.allen11@gmail.com>
2019-09-23 13:10:51 -0700

    added content to 'project.js' file in server/models.  'projectLead' and 'teamMembers' may need refactoring.  I'll get back to that soon.

---
+++ server/models/project.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const employee = require('./employee')
+const ProjectSchema = new mongoose.Schema({
+    title: { 
+        type: String, 
+        minlength: [3, 'Project name must be at least 3 characters long.'], 
+        required: [true, "Please provide a project title."], 
+        unique: true 
+    },
+    description: { 
+        type: String,
+        required: [true, "Please provide a project description."], 
+        minlength: [3, 'Project description must be at least 3 characters long.']
+    },
+    dueDate:{
+        type: Date,
+        required: [true, "Please provide a target completion date."]
+    },
+    progress:{
+        type: Number,
+    },
+    isComplete:{
+        type: Boolean,
+    },
+    projectLead: employee.EmployeeSchema,
+    teamMembers: [employee.EmployeeSchema]
+}, {timestamps: true });
+ProjectSchema.plugin(uniqueValidator, { message: 'Project {PATH} must be unique.'});
+mongoose.model('Project', ProjectSchema);
