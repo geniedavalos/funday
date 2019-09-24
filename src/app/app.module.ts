e58e15dab807430d62da8da06f0b6bdaaead00c1
Wade Allen <wade.allen11@gmail.com>
2019-09-24 16:33:29 -0700

    barrelled resolvers and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DevTeamComponent } from './dev-team/dev-team.component';
 import { SiteInfoComponent } from './site-info/site-info.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
-import { ManagerDashboardComponent } from './dashboard/manager-dashboard/manager-dashboard.component';
-import { EmployeeDashboardComponent } from './dashboard/employee-dashboard/employee-dashboard.component';
+import { DevTeamComponent } from './dev-team/dev-team.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
 
+import * as fromDashboards from './dashboard';
 import * as fromEmployees from './employees';
+import * as fromCompanies from './companies';
 import * as fromResolvers from './resolvers';
 import * as fromProjects from './projects';
 import * as fromTasks from './tasks';
-import * as fromCompanies from './companies';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    ...fromDashboards.components,
     ...fromEmployees.components,
+    ...fromCompanies.components,
     ...fromProjects.components,
     ...fromTasks.components,
-    ...fromCompanies.components,
-    HomeComponent,
-    DevTeamComponent,
     SiteInfoComponent,
-    DashboardComponent,
-    EmployeeDashboardComponent,
-    ManagerDashboardComponent
+    DevTeamComponent,
+    HomeComponent,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,9 +35,7 @@ import * as fromCompanies from './companies';
     HttpClientModule,
   ],
   providers: [
-    fromResolvers.EmployeeResolver,
-    fromResolvers.ProjectResolver,
-    fromResolvers.TaskResolver
+    ...fromResolvers.resolvers
   ],
   bootstrap: [AppComponent]
 })
