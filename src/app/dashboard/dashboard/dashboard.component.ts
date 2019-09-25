fece119143d9cb0a6cea7bc8dfeb7aa00489ea3d
Eric <eajisaka@gmail.com>
2019-09-25 09:29:14 -0700

    Some routing

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  isManager: Boolean;
   constructor() { }
 
   ngOnInit() {
+    this.isManager = false;
   }
 
 }
