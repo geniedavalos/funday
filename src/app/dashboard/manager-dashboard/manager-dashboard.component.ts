eb38a7488d9e93834f319558453a40554ca6736c
Eric <eajisaka@gmail.com>
2019-09-25 12:25:52 -0700

    Added logic to manager-dashboard component

--- src/app/dashboard/manager-dashboard/manager-dashboard.component.ts
+++ src/app/dashboard/manager-dashboard/manager-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models';
 
 @Component({
   selector: 'app-manager-dashboard',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manager-dashboard.component.css']
 })
 export class ManagerDashboardComponent implements OnInit {
-
+  newProject = new Project();
+  addedTeamMembers: any [];
+  addedIds: any [];
+  newMember: any;
+  random: string;
   constructor() { }
 
   ngOnInit() {
+    this.addedIds = [];
+    this.addedTeamMembers = [];
+    this.random = 'random-string';
+  }
+
+  onAdd() {
+    console.log('Inside onAdd() for teammembers');
+    console.log(this.newMember);
+
+    for (const  members of this.newMember) {
+      const split = members.split('-');
+      this.addedIds.push(split[0]);
+      this.addedTeamMembers.push(split[1]);
+    }
+    console.log('Ids are: ' + this.addedIds);
+    console.log('Names are: ' + this.addedTeamMembers);
+
+  }
+
+  onSubmit() {
+    console.log('Inside onSubmit() for form');
+    console.log('Submitting: ' + this.newProject.title + ', ' + this.newProject.description);
+
   }
 
 }
