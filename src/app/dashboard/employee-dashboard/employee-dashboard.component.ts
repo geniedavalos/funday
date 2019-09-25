29e97af83fe905700de916eb1aeea70203b1a706
Eric <eajisaka@gmail.com>
2019-09-24 17:57:55 -0700

    test

--- src/app/dashboard/employee-dashboard/employee-dashboard.component.ts
+++ src/app/dashboard/employee-dashboard/employee-dashboard.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-dashboard.component.css']
 })
 export class EmployeeDashboardComponent implements OnInit {
+  noteDescription: String;
+  theId = 'Random';
 
   constructor() { }
-
   ngOnInit() {
   }
 
+  onSubmit(id:String) {
+    console.log('Inside onSubmit()');
+    console.log(id);
+    console.log(this.noteDescription);
+  }
 }
