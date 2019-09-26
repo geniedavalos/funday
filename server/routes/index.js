e892e58ac54a7099f99d2305813c69da479eb809
phatluong123 <phat.luong1128@gmail.com>
2019-09-26 16:08:30 -0700

    adding model, route, controller for note progress

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,14 @@ const catchallRoute = require('./catchall.routes');
 const employeeRoutes = require('./employee.routes');
 const projectRoutes = require('./project.routes');
 const taskRoutes = require('./task.routes');
+const noteRoutes = require('./note.routes');
 const authRoutes = require('./auth.routes');
 const companyRoutes = require('./company.routes');
 apiRouter.use('/employees', employeeRoutes)
   .use('/projects', projectRoutes)
   .use('/tasks', taskRoutes)
-  .use('/companies', companyRoutes);
+  .use('/companies', companyRoutes)
+  .use('/notes', noteRoutes);
 router.use('/api', apiRouter)
   .use('/auth', authRoutes)
   .use(catchallRoute);
