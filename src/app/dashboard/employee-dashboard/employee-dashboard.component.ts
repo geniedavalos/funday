f98cb07b0b53de74f3da77579987e660f56a5231
Wade Allen <wade.allen11@gmail.com>
2019-09-26 11:04:23 -0700

    Added data propegation to project details page.

--- src/app/dashboard/employee-dashboard/employee-dashboard.component.ts
+++ src/app/dashboard/employee-dashboard/employee-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Employee } from 'src/app/models';
+import { Employee, Task } from 'src/app/models';
+import { TaskService } from 'src/app/services';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -7,17 +9,27 @@ import { Employee } from 'src/app/models';
   styleUrls: ['./employee-dashboard.component.css']
 })
 export class EmployeeDashboardComponent implements OnInit {
+  tasks: Task[];
   noteDescription: String;
   @Input() currentUser: Employee;
   theId = 'Random';
 
-  constructor() { }
+  constructor(
+    private readonly taskService: TaskService,
+  ) { }
   ngOnInit() {
+    this.getTasks();
   }
 
   onSubmit(id:String) {
     console.log('Inside onSubmit()');
     console.log(id);
     console.log(this.noteDescription);
+  }
+
+  getTasks(){
+    this.taskService.getEmployeeTasks(this.currentUser._id).subscribe(tasks => {
+      this.tasks = tasks;
+    })
   }
 }
